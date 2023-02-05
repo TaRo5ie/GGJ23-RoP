bf966581e29e03e5daa7a57b76aa15828dfe1c90
peanutscratch <nsshadow96@gmail.com>
2023-02-05 03:54:20 -0500

    Scriptable Objects and Animations
    Made all 12 scriptable objects with life stages, ID #s, and associated Sprites. Also added in all art assets available at the time, including walk cycle animations

--- GGJ23-RoP/Assets/Scripts/MemoryObject.cs
+++ GGJ23-RoP/Assets/Scripts/MemoryObject.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "MemoryObject", menuName = "ScriptableObjects/MemoryObject", order = 1)]
 public class MemoryObject : ScriptableObject
 {
+    public enum LifeState
+    {Child, Young, MiddleAged, Old}
 
+    [HideInInspector]
+    public int choiceNumber;
+    public int objectID;
+    public LifeState lifeState;
+    public Sprite memoryPreview;
     
 }
