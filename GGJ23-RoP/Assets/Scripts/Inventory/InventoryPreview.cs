9f8884ce8a4c3742e8abbac6dbd05f171cc0d12a
peanutscratch <nsshadow96@gmail.com>
2023-02-09 20:51:23 -0500

    Inventory Preview
    Added memory preview for inventory memories and fixed bug where blank memories could be previewed

--- GGJ23-RoP/Assets/Scripts/Inventory/InventoryPreview.cs
+++ GGJ23-RoP/Assets/Scripts/Inventory/InventoryPreview.cs
@@ -2,34 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class PreviewTrigger : MonoBehaviour
+public class InventoryPreview : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-
+    
     public GameObject memoryPreview;
+    public Vector3 customOffset;
+    private Vector3 originalOffset;
     public MemoryObject blank;
     private Image previewImage;
     private SelectMapMemory mapMem;
 
     void Start()
     {
+        
         mapMem = GetComponent<SelectMapMemory>();
         memoryPreview = FindObjectOfType<MemoryPreview>(true).gameObject;
         previewImage = memoryPreview.GetComponent<Image>();
-        // I added this in case I forgot to set the object
+        originalOffset = memoryPreview.GetComponent<MemoryPreview>().offset;
+        // I added t$$anonymous$$s in case I forgot to set the tooltip object
         if (memoryPreview!= null)
         {
             memoryPreview.SetActive(false);
         }
     }
 
-    public void OnMouseEnter()
+    public void OnPointerEnter(PointerEventData eventData)
     {
+        
+        memoryPreview.GetComponent<MemoryPreview>().offset = customOffset;
+        //Debug.Log("Entered Button "+this.name);
         // Same here
         if (memoryPreview!= null)
         {
-            previewImage.sprite = mapMem.memory.memoryPreview;
-            
+            previewImage.sprite = GetComponent<Image>().sprite;
             if(previewImage.sprite != blank.memoryPreview)
             {
                 memoryPreview.SetActive(true);
@@ -37,8 +44,10 @@ public class PreviewTrigger : MonoBehaviour
         }
     }
 
-    public void OnMouseExit()
+    public void OnPointerExit(PointerEventData eventData)
     {
+        
+        memoryPreview.GetComponent<MemoryPreview>().offset = originalOffset;
         // and same here
         if (memoryPreview!= null)
         {
